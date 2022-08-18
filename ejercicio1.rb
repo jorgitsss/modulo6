5fdb5d2e4b11cdcf77ccdf156a89ff2494e43a4e
Jorge Godoy Vergara <jorge.godoy.vergara@gmail.com>
2022-08-17 20:50:04 -0400

    ejercicio individual m06 abpro1

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -9,13 +9,39 @@
     ● El sistema debe imprimir en Pantalla “Hola te Recomendados esto para tu hijo “(El mensaje del resultado de la validación)”
 =end
 
-edad = 10   # solicito la edad al usuario
-=gets.chomp.to_i  # obtengo variable le quito el retorno y la dejo como entero
-if edad <=4 #condicion uno
+puts "ingrese edad"   # solicito la edad al usuario
+ edad = gets.chomp.to_i  # obtengo variable le quito el retorno y la dejo como entero
+=begin if edad <=4 #condicion uno
     puts "limite izquierdo de la condicion"
 elsif edad >= 6 #condicion dos
     puts "limite derecho de la condicion"
 else
     puts "entre condicion uno y dos"
     puts "entre condicion 4 y 6 años"
+end 
+=end
+puts "================================="
+if edad <= 3
+    puts " "
+    puts ">No debe tener contacto alguno con tecnología<"
+    puts " "
+    puts " "
+
+elsif edad <= 6
+    puts " "
+    puts ">El uso debe ser restringido a 1 hora diaria<"
+    puts " "
+    puts " "
+
+elsif edad < 18
+    puts " "
+    puts ">El uso debe ser restringido a 2 horas diarias<"
+    puts " "
+    puts " "
+
+else edad >= 18
+    puts " "
+    puts "Un adulto no requiere restricción de uso"
+    puts " "
+    puts " "
 end
